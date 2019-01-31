497364eef9ab49ac06e406a0c5853a0b775bcf36
Arya <yafang1826@163.com>
2019-01-31 15:25:30 +0800

    init

---
+++ backend/src/main/scala/com/neo/sk/breakout/http/HttpService.scala
@@ -0,0 +1,98 @@
+package com.neo.sk.breakout.http
+
+import java.net.URLEncoder
+
+import akka.actor.{ActorSystem, Scheduler}
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.ws.Message
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.stream.Materializer
+import akka.stream.scaladsl.Flow
+import akka.util.Timeout
+import com.neo.sk.breakout.common.AppSettings
+import akka.actor.typed.scaladsl.AskPattern._
+
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import com.neo.sk.breakout.shared.ptcl.ErrorRsp
+
+import scala.util.Random
+
+/**
+  * Created by hongruying on 2018/3/11
+  */
+trait HttpService
+  extends ResourceService
+    with ServiceUtils {
+
+  import akka.actor.typed.scaladsl.AskPattern._
+  import com.neo.sk.utils.CirceSupport._
+  import io.circe.generic.auto._
+  import io.circe._
+
+  implicit val system: ActorSystem
+
+  implicit val executor: ExecutionContextExecutor
+
+  implicit val materializer: Materializer
+
+  implicit val timeout: Timeout
+
+  implicit val scheduler: Scheduler
+
+
+  def platEnterRoute: Route = path("playGame"){
+    parameter(
+      'playerId.as[String],
+      'playerName.as[String],
+      'accessCode.as[String],
+      'roomId.as[Long].?
+    ) {
+      case (playerId, playerName, accessCode, roomIdOpt) =>
+        redirect(s"/tank/game/#/playGame/${playerId}/${URLEncoder.encode(playerName, "utf-8")}" + roomIdOpt.map(s => s"/$s").getOrElse("") + s"/$accessCode",
+          StatusCodes.SeeOther
+        )
+
+    }
+  } ~ path("watchGame") {
+    parameter(
+      'roomId.as[Long],
+      'accessCode.as[String],
+      'playerId.as[String].?
+    ) {
+      case (roomId, accessCode, playerIdOpt) =>
+        redirect(s"/tank/game/#/watchGame/${roomId}" + playerIdOpt.map(s => s"/$s").getOrElse("") + s"/$accessCode",
+          StatusCodes.SeeOther
+        )
+
+    }
+  } ~ path("watchRecord") {
+    parameter(
+      'recordId.as[Long],
+      'playerId.as[String],
+      'frame.as[Long],
+      'accessCode.as[String]
+    ) {
+      case (recordId, playerId, frame, accessCode) =>
+        redirect(s"/tank/game/#/watchRecord/${recordId}/${playerId}/${frame}/${accessCode}",
+          StatusCodes.SeeOther
+        )
+    }
+  }
+
+
+
+
+  lazy val routes: Route = pathPrefix(AppSettings.rootPath){
+    resourceRoutes ~ platEnterRoute ~
+      (pathPrefix("game") & get){
+        pathEndOrSingleSlash{
+          getFromResource("html/admin.html")
+        }
+      }
+  }
+
+
+
+
+}
---
+++ backend/src/main/scala/com/neo/sk/breakout/models/package.scala
@@ -0,0 +1,15 @@
+package com.neo.sk.breakout
+
+/**
+  * Created by hongruying on 2018/10/18
+  */
+package object models {
+
+  case class TankGameUserInfo(
+                     userId:String,
+                     nickName:String,
+                     name:String,
+                     isFlatUser:Boolean
+                     )
+
+}
